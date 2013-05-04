8b269620a7815a5ea11216c3aea0ecb7f665063e
Christopher Erwin <chrisferwin@gmail.com>
2013-05-04 15:02:41 -0700

    Add cell state modification methods
    Add: Map::get_cell()
    Add: Map::set_cell()

--- caSandbox.py
+++ caSandbox.py
@@ -3,3 +3,17 @@ import map
 m = map.Map()
 
 m.print_cells()
+
+def print_cells():
+    # Print some cells for testing
+    print(m.get_cell(0, 4))
+    print(m.get_cell(4, 0))
+    print(m.get_cell(0, 0))
+
+print_cells()
+# Update the cells
+m.set_cell(0, 4, False)
+m.set_cell(4, 0, False)
+m.set_cell(0, 0, True)
+print_cells()
+m.print_cells()
--- map.py
+++ map.py
@@ -11,8 +11,35 @@ class Map:
             , [True, False, False, False, False]
         ]
 
+    def get_cell(self, x, y):
+        """ Get the state of a cell.
+
+            Returns the cell's state
+
+            Variables:
+            x       int     X coordinate of the cell
+            y       int     Y coordinate of the cell
+        """
+        return self.cells[x][y]
+
+    def set_cell(self, x, y, state):
+        """ Set the state of a cell.
+
+            Returns the cell's new state
+
+            Variables:
+            x       int     X coordinate of the cell
+            y       int     Y coordinate of the cell
+            state   bool    The cell's new state
+        """
+        self.cells[x][y] = state
+
+        return self.cells[x][y]
+
     def print_cells(self):
         """ Prints the entire map to the screen. """
+        print()
+        print()
         for row in self.cells:
             for cell in row:
                 if cell is True:
@@ -20,3 +47,5 @@ class Map:
                 else:
                     sys.stdout.write('O')
             sys.stdout.write('\n')
+        print()
+        print()
